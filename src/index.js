8686d0d3480d6880afa75c8b99bb65af8092f862
Farwa-Qasim-512 <farwaqasim461@gmail.com>
2021-01-20 22:56:01 +0500

    Final Project Submission

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
-//import Search from './components/Search/Search.component';
-//import Movie from './components/Movie/Movie.component';
-////
-// 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
